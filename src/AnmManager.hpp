83ecf21b4beb10b394de954a11e40895245786d6
Swiizyu <298737408+Swiizyu@users.noreply.github.com>
2026-08-20 15:02:49 +0000

    Revert "Experiment: AnmVm::Initialize out-of-line (header body reserved inline slots in every TU; orig TUs reserve none)"
    This reverts commit 9309bce4fe4957e4130e0a6d25e6de377545058e.

--- src/AnmManager.hpp
+++ src/AnmManager.hpp
@@ -385,7 +385,16 @@ C_ASSERT(sizeof(AnmVmBase) == 0x208);
 
 struct AnmVm : AnmVmBase
 {
-    void Initialize();
+    void Initialize()
+    {
+        memset(this, 0, sizeof(AnmVmBase));
+        this->scale.x = 1.0f;
+        this->scale.y = 1.0f;
+        this->color1.d3dColor = COLOR_WHITE;
+        D3DXMatrixIdentity(&this->matrix1);
+        this->flags = 7;
+        this->currentTimeInScript.Initialize();
+    }
 
     ZunBool IsVisible()
     {
